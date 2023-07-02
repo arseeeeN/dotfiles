26407539636518b0dc3ee892c1b61eae6324c31c
Arsenije Jevric <arsenije99@hotmail.ch>
2023-07-02 11:54:07 +0200

    add telescope_file_picker

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -22,3 +22,6 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+vim.keymap.set("n", "<leader>fe", ":Telescope file_browser<CR>", {})
+vim.keymap.set("n", "<leader>feb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", {})
