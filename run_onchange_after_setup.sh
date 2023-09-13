989e669562f81b32613494b19350db5e83beb5af
Arsenije Jevric <arsenije99@hotmail.ch>
2023-09-13 15:20:46 +0200

    Fix function hoisting

--- run_onchange_after_setup.sh
+++ run_onchange_after_setup.sh
@@ -1,38 +1,40 @@
 #!/bin/sh
 
-# Install yay if it doesn't exist
-if ! command -v yay &> /dev/null
-then
-    pacman -S --needed git base-devel
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    cd ..
-    rm -rf ./yay
-fi
+main() {
+    # Install yay if it doesn't exist
+    if ! command -v yay &> /dev/null
+    then
+        pacman -S --needed git base-devel
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+        cd ..
+        rm -rf ./yay
+    fi
 
-# Setup env vars
-vars_file_path=~/.config/nushell/vars.nu
-if [[ ! -f "$vars_file_path" ]]; then
-    setup_env_headless
-fi
+    # Setup env vars
+    vars_file_path=~/.config/nushell/vars.nu
+    if [[ ! -f "$vars_file_path" ]]; then
+        setup_env_headless
+    fi
 
-# Update packages and install nushell
-yay -Syu --needed nushell
+    # Update packages and install nushell
+    yay -Syu --needed nushell
 
-# Install all other packags
-nu ./install_packages.nu
+    # Install all other packags
+    nu ./install_packages.nu
+}
 
 # TODO: Potentially move the env var creation to a nushell script and only do the bare minimum setup in bash
 # TODO: Find a way to recognize if a new variable got added/is missing in the existing vars.nu file and prompt that one
-setup_env_headless(){
+setup_env_headless() {
     printf "\055\055 Installation Options\n"
     printf "Do you want to install all desktop related packages? (window manager, GUI programms, etc.)\n"
     printf "If you choose no then a headless install will be performed.\n"
     prompt_env CHEZMOI_HEADLESS
 }
 
-prompt_env(){
+prompt_env() {
     while true; do
         printf "[y/n]: "
         read -r bool </dev/tty
@@ -52,8 +54,10 @@ prompt_env(){
     done
 }
 
-set_env(){
+set_env() {
     printf "Setting $1=$2\n"
     echo "\$env.$1 = $2" >> $vars_file_path
     export $1=$2
 }
+
+main "$@"
